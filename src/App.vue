dd7ac2e02006911260f9e85fd113025f716a3988
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-05-19 08:55:34 +0300

    Show product list

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <product-list/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ProductList from './components/ProductList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blank',
+  data () {
+    return {
+      products: [
+        { id: 1, title: 'Tomatoes', price: 500 },
+        { id: 2, title: 'Rice', price: 1500 },
+        { id: 3, title: 'Beans', price: 1000 }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
